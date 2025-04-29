ff281915c23fbcedd50326cfdc101adbe8c2d0d8
sheepdog9678 <sheepdog13@naver.com>
2025-04-30 03:04:37 +0900

    feat : 라우터, icon 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,11 @@
 import "./App.css";
-import { useCounter } from "./store/useCounter";
+import Router from "./router/Router";
 
 function App() {
-  const { count, increase, decrease, reset } = useCounter();
   return (
-    <div className="p-4 text-center">
-      <h1 className="text-2xl mb-4">Count: {count}</h1>
-      <div className="flex justify-center gap-2">
-        <button
-          onClick={increase}
-          className="bg-green-500 text-white px-4 py-2 rounded"
-        >
-          +
-        </button>
-        <button
-          onClick={decrease}
-          className="bg-red-500 text-white px-4 py-2 rounded"
-        >
-          -
-        </button>
-        <button
-          onClick={reset}
-          className="bg-gray-500 text-white px-4 py-2 rounded"
-        >
-          Reset
-        </button>
-      </div>
-    </div>
+    <>
+      <Router />
+    </>
   );
 }
 
